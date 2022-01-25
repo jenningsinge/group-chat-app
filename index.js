b154c3d82ea474a17870633fa85d2b028ab7ae1d
jenningsinge <jennings_inge@utexas.edu>
2022-01-25 15:29:10 -0600

    Added functionality for saving sent messages to the server (no database yet). Added functionality for creating new channels.

--- index.js
+++ index.js
@@ -6,15 +6,37 @@ const path = require("path");
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const messages = [];
+const channels = ["Welcome", "Test1", "Test2"];
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  if (req.method === "POST") console.log(req.body);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
 
+app.get("/channels", (req, res) => {
+  res.send(channels);
+});
+
+app.post("/channels", (req, res) => {
+  channels.push(req.body.ch);
+  res.send(req.body);
+});
+
+io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    messages.push(msg);
     io.emit("chat message", msg);
   });
 
