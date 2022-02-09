123b6b569eecc2771443818554ccad7375e7cd8e
jenningsinge <jennings_inge@utexas.edu>
2022-02-08 20:00:01 -0600

    fixed setting port

--- index.js
+++ index.js
@@ -174,6 +174,6 @@ io.on("connection", (socket) => {
 });
 
 const port = process.env.PORT || 3000;
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
